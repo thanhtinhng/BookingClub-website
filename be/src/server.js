5fc9971ff761030f45c088182b600ddc217b6f22
nhsmai04 <mr.nguyenhandsome04@gmail.com>
2026-02-25 17:30:38 +0700

    modified test model

--- be/src/server.js
+++ be/src/server.js
@@ -1,11 +1,18 @@
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import express from 'express';
+import noteRoutes from './routers/notesRoutes.js';
+
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5001;
-dotenv.config();
+
 connectDB();
-console.log(process.env.MONGO_URI);
+app.use(express.json());
+app.use("/api/notes", noteRoutes);
+
+
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
