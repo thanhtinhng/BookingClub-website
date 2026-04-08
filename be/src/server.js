6d032a8dd1d449b3774b9ad787762888190c9a22
nhsmai04 <mr.nguyenhandsome04@gmail.com>
2026-04-08 10:11:11 +0700

    update base project

--- be/src/server.js
+++ be/src/server.js
@@ -7,15 +7,17 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
+console.log(process.env.MONGO_URI);
 connectDB();
 app.use(express.json());
 app.use("/api/notes", noteRoutes);
-
+app.use('/{*any}', (req, res) => {
+ res.status(404).json({ error: 'Route not found' });
+});
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 app.listen(PORT, () => {
-  console.log("Server is running on port",+ PORT);
+  console.log("Server is running on port", PORT);
 });
